66098247a1aa9aa758857153f4b527538c34118c
romashka85 <duratsia@students.softuni.bg>
2018-11-27 14:54:00 +0200

    lub Ajax

---
+++ JS Apps/01. Lab AJAX/04. Phonebook/phonebook.js
@@ -0,0 +1,49 @@
+$(function () {
+    $('#btnLoad').on('click', loadContacts);
+    $('#btnCreate').on('click', createContacts);
+    let baseServiseUrl = "https://phonebook-yulia.firebaseio.com/phonebook";
+    
+    function loadContacts() {
+        $('#phonebook').empty();
+        $.get(baseServiseUrl + '.json')
+            .then(displayContacts)
+            .catch(displayError);
+    }
+    
+    function displayContacts(contacts) {            //как се разбира какво точно търсиме??
+     for (let key in contacts){
+         let person = contacts[key]['person'];
+         let phone = contacts[key]['phone'];
+         let li = $('<li>');
+         li.text(person + ': ' + phone + ' ');
+         $('#phonebook').append(li);
+         li.append($(`<button>Delete</button>`).on('click', deleteContact.bind(this, key)));
+     }
+    }
+    function displayError(err) {
+        $('#phonebook').append($('<li>Error</li>'))
+    }
+    
+    function createContacts() {
+        let newContactJASON = JSON.stringify({
+            person: $('#person').val(),
+            phone: $('#phone').val()
+        });
+        $.post(baseServiseUrl + '.json', newContactJASON)
+            .then(loadContacts)
+            .catch(displayError);
+        $('#person').val('');
+        $('#phone').val('');
+
+    }
+    function deleteContact(key) {
+        let request = {
+            method: 'DELETE',
+            url: baseServiseUrl + '/' + key + '.json'
+        };
+        $.ajax(request)
+            .then(loadContacts)
+            .catch(displayError);
+    }
+
+});
